062861ff145e08696138439ac3d235518fbcc139
Fraser Watson <fraser.watson@mac.com>
2020-08-19 10:49:11 +0100

    Added room connecting and disconnecting

--- editor.py
+++ editor.py
@@ -32,6 +32,9 @@ mouseY = 0
 
 SPEED = 3 # 4
 
+connectMode = False
+connectedRoom1 = None
+
 doorColours = [
     WHITE,
     RED,
@@ -70,6 +73,9 @@ def validateRoom(x, y, map, type):
             if room.doors[(i + 2) % 4] >= 0 and type[0][i] == -1:
                 return False
 
+            if room.doors[(i + 2) % 4] == -1 and type[0][i] >= 0:
+                return False
+
     return True
 
     '''if map.getRoom(x + doorToDisp[0][0], y + doorToDisp[0][1]) is not None:
@@ -81,10 +87,23 @@ def validateRoom(x, y, map, type):
             return False'''
 
 def closeHandler(map):
-    with open(MAP_FILE, 'w') as f:
-        f.write(json.dumps(map.toDict(), indent=4))
+    #with open(MAP_FILE, 'w') as f:
+        #f.write(json.dumps(map.toDict(), indent=4))
 
     sys.exit()
+
+def findConnectingDoor(room1, room2):
+    diffX = room2.x - room1.x
+    diffY = room2.y - room1.y
+
+    counter = -1
+
+    for door in doorToDisp:
+        counter += 1
+        if door[0] == diffX and door[1] == diffY:
+            return counter
+
+    return -1
 
 clock = pygame.time.Clock()
 
@@ -126,6 +145,52 @@ while True:
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1: # Left click
+
+                if connectMode:
+                    room = gamemap.getRoom(selectedX, selectedY)
+                    if room is None or room.type == 0: continue
+                    if connectedRoom1 is None:
+                        connectedRoom1 = room
+
+                    else:
+                        door = findConnectingDoor(connectedRoom1, room)
+                        if door == -1: continue
+
+                        room.doors[(door + 2) % 4] = -1 if connectedRoom1.doors[door] != -1 else 0
+
+                        possibleTypes = core.roomTypes[1:]
+                        possibleTypes = list(filter(lambda a: validateRoom(connectedRoom1.x, connectedRoom1.y, gamemap, a),
+                                                    zip(possibleTypes, range(len(possibleTypes)))))
+
+                        newDoors = possibleTypes[0][0][:]
+                        for x in range(4):
+                            if newDoors[x] == 0 and connectedRoom1.doors[x] >= 0:
+                                newDoors[x] = connectedRoom1.doors[x]
+
+                        connectedRoom1.doors = newDoors
+                        connectedRoom1.type = possibleTypes[0][1] + 1
+
+                        room.doors[(door + 2) % 4] = ((room.doors[(door + 2) % 4] + 1) % 2) - 1
+
+                        possibleTypes = core.roomTypes[1:]
+                        possibleTypes = list(
+                            filter(lambda a: validateRoom(room.x, room.y, gamemap, a),
+                                   zip(possibleTypes, range(len(possibleTypes)))))
+
+                        newDoors = possibleTypes[0][0][:]
+                        for x in range(4):
+                            if newDoors[x] == 0 and room.doors[x] >= 0:
+                                newDoors[x] = room.doors[x]
+
+                        room.doors = newDoors
+                        room.type = possibleTypes[0][1] + 1
+
+                        connectedRoom1 = None
+                        connectMode = False
+
+
+                    continue
+
                 room = gamemap.getRoom(selectedX, selectedY)
                 if room == None or selectedDoor == -1: continue
                 disp = doorToDisp[selectedDoor]
@@ -147,7 +212,7 @@ while True:
 
             elif event.button == 3:
                 room = gamemap.getRoom(selectedX, selectedY)
-                if room is not None and selectedDoor == -1 and room.type > 0:
+                if room is not None and selectedDoor == -1 and room.type > 0 and connectMode is False:
                     possibleTypes = core.roomTypes[1:]
                     possibleTypes = list(filter(lambda a: validateRoom(selectedX, selectedY, gamemap, a),
                                                 zip(possibleTypes, range(len(possibleTypes)))))
@@ -169,6 +234,10 @@ while True:
                     room.doors = newDoors
                     room.type = possibleTypes[currentIndex][1] + 1
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_c:
+                connectMode = not connectMode
+
 
     keys = pygame.key.get_pressed()
 
@@ -187,12 +256,19 @@ while True:
     if keys[pygame.K_ESCAPE]:
         closeHandler(gamemap)
 
-
     screen.fill(WHITE)
 
     for room in gamemap.rooms:
         if room.x == selectedX and room.y == selectedY and selectedDoor == -1:
             pygame.draw.rect(screen, GREY,
+                             pygame.Rect(((room.x * EDIT_ROOM_SIZE) - cameraX), ((room.y * EDIT_ROOM_SIZE) - cameraY),
+                                         EDIT_ROOM_SIZE, EDIT_ROOM_SIZE))
+
+        if (room.x == selectedX and room.y == selectedY and selectedDoor == -1 and connectMode and
+            ((connectedRoom1 is None or findConnectingDoor(connectedRoom1, room) != -1)
+            or connectedRoom1 is None)) or room == connectedRoom1:
+
+            pygame.draw.rect(screen, GREEN,
                              pygame.Rect(((room.x * EDIT_ROOM_SIZE) - cameraX), ((room.y * EDIT_ROOM_SIZE) - cameraY),
                                          EDIT_ROOM_SIZE, EDIT_ROOM_SIZE))
 
